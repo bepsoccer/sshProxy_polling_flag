039cff35b6cbd22a66adda3ad1ed6342d1bd38d7
bepsoccer <brad@parker1723.com>
2018-03-20 16:34:43 -0500

    Capture Just error message

--- download.js
+++ download.js
@@ -4,17 +4,22 @@ var fs = require('fs');
 var sftp = new Client();
 
 sftp.connect({
-    host: process.env.SERVER,
-    port: '22',
-    username: process.env.UN,
-    password: process.env.PASS
-}).then(() => {
-    sftp.get('results.xml')
-	.then((stream) => {
-  		stream.pipe(fs.createWriteStream('results.xml'));
-  		sftp.end();
-	})
+  host: process.env.SERVER,
+  port: '22',
+  username: process.env.UN,
+  password: process.env.PASS
+})
+.then(() => {
+  return sftp.put('results.xml', 'resultsUpload.xml');
+})
+.then(() => {
+  return sftp.get('results.xml');
+})
+.then((stream) => {
+  stream.pipe(fs.createWriteStream('results.xml'));
+  sftp.end();
 })
 .catch((err) => {
-    console.log(err, 'catch error');
+    console.log(err.message);
+    sftp.end();
 });
