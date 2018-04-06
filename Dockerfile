48e47fa56e2f4388123b8f14342694148e617c41
bepsoccer <brad@parker1723.com>
2018-04-05 20:26:24 -0700

    working docker nodejs server

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM node:9
-RUN apt-get update && apt-get install -y cron
 WORKDIR /var/poller
 COPY /src/package*.json ./
 RUN npm install
 COPY /src/. ./
-COPY poller-cron /etc/cron.d/poller-cron
-RUN chmod 0644 /etc/cron.d/poller-cron
-CMD touch /var/log/cron.log && cron && tail -f /var/log/cron.log
+CMD [ "npm", "start" ]
--- src/download.js
+++ src/download.js
@@ -1,39 +1,49 @@
-require('dotenv').load();
+var settings = require('./settings.json');
 var Client = require('ssh2-sftp-client');
 var fs = require('fs');
 var sftp = new Client();
+var sleep = require('sleep');
 
-sftp.connect({
-  host: process.env.SERVER,
-  hostHash: 'md5',
-  hostVerifier: function(hashedKey) {
-    if (process.env.HASH === "") {
-      //No expected hash so save save what was received from the host (hashedKey)
-      //console.log("Server hash: " + hashedKey);
-      return true;
-    } else if (hashedKey === process.env.HASH) {
-      //console.log("Hash values matched");
-      return true;
-    }
-    //Output the failed comparison to the console if you want to see what went wrong
-    //console.log("Hash values: Server = " + hashedKey + " <> Client = " + process.env.HASH);
-    return false;
-  },
-  port: '22',
-  username: process.env.UN,
-  password: process.env.PASS
-})
-.then(() => {
-  return sftp.put('results.xml', 'resultsUpload.xml');
-})
-.then(() => {
-  return sftp.get('results.xml');
-})
-.then((stream) => {
-  console.log("no proxy in place");
-  sftp.end();
-})
-.catch((err) => {
-    console.log(err.message);
+function poller(target){
+  sleep.sleep(10);
+  sftp.connect({
+    host: target,
+    hostHash: 'md5',
+    hostVerifier: function(hashedKey) {
+      if (settings.HASH === "") {
+        //No expected hash so save save what was received from the host (hashedKey)
+        console.log("Server hash: " + hashedKey);
+        return true;
+      } else if (hashedKey === settings.HASH) {
+        //console.log("Hash values matched");
+        return true;
+      }
+      //Output the failed comparison to the console if you want to see what went wrong
+      console.log("Hash values: Server = " + hashedKey + " <> Client = " + setting.HASH);
+      return false;
+    },
+    port: '22',
+    username: settings.UN,
+    password: settings.PASS
+  })
+  .then(() => {
+    return sftp.put('results.xml', 'resultsUpload.xml');
+  })
+  .then(() => {
+    return sftp.get('results.xml');
+  })
+  .then((stream) => {
+    console.log("no proxy in place");
     sftp.end();
+    poller(target);
+  })
+  .catch((err) => {
+      console.log(err.message);
+      sftp.end();
+      poller(target);
+  });
+}
+
+settings.sshServers.forEach(function(target) {
+  poller(target);
 });
